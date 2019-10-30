b12b7bfb8729475a509ceb600ce775d86daa261a
talk2coded <emmanuelsteve43@gmail.com>
2019-10-30 16:02:49 +0100

    implemented firebase utils, included ability to store authenticated users into our firestore data

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Header from './components/header/header.component';
 
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 
 
@@ -27,9 +27,21 @@ constructor() {
 unsubscribedFromAuth = null
 
 componentDidMount() {
-  this.unsubscribedFromAuth = auth.onAuthStateChanged(user => {
-    this.setState({ currentUser: user});
-  })
+  this.unsubscribedFromAuth = auth.onAuthStateChanged(async userAuth => {
+    if(userAuth) {
+      const userRef = await createUserProfileDocument(userAuth);
+
+      userRef.onSnapshot(snapShot => {
+        this.setState({
+          currentUser:{
+            id: snapShot.id,
+            ...snapShot.data()
+          }
+        })
+      });
+    }
+    this.setState({currentUser: userAuth});
+  });
 }
 
 componentWillUnmount() {
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
 
+import SignUp from '../../components/sign-up/sign-up.component';
+
 import './sign-in-and-sign-up.style.scss';
 
 
@@ -9,6 +11,7 @@ const SignInAndSignUp = () => (
 
     <div className='sign-in-and-sign-up'>
     <SignIn/>
+    <SignUp/>
     </div>
 );
 
