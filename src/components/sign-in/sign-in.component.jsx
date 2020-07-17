9605b2cb780d652998b80889cca8c912ca62a976
talk2coded <emmanuelsteve43@gmail.com>
2020-07-17 12:14:30 +0100

    fixed a bug

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -44,7 +44,7 @@ class SignIn extends React.Component{
     render() {
         return(
             <div className='sign-in'>
-                <h2>I already have</h2>
+                <h2>I already have an account</h2>
                 <span>Sign in with your email and password</span>
                 <form onSubmit={this.handleSubmit}>
                     <FormInput name='email'  label="email" value={this.state.email}  required  handleChange={this.handleChange} />
